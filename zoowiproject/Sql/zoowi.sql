d2f8113b640d4e80c80df6e8b9b275a9f53f88d7
kumarsunnyil <kumar.sunnyil@gmail.com>
2015-02-13 07:16:25 +0530

    Adding Sql Files for the Database.
    This folder will not be available or moved to the Implementation folder,
    Rather the SQL Script will be executed in the Database.

---
+++ zoowiproject/Sql/zoowi.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE ZOOWI;
+USE DATABASE ZOOWI 
+
+DROP TABLE IF EXISTS `zoowi`.`user`;
+CREATE TABLE  `zoowi`.`user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
