1c5a6ccec1526328468acf6e450d149caaea5266
kumarsunnyil <kumar.sunnyil@gmail.com>
2015-03-03 21:46:16 +0530

    column 1 modifie
    column 1 modifie

--- hoauthmasteryii/protected/views/layouts/column1.php
+++ hoauthmasteryii/protected/views/layouts/column1.php
@@ -1,6 +1,16 @@
 <?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
+<?php
+	if (!Yii::app()->user->isGuest) {
+		$this->beginContent('//layouts/main');
+	} else
+		$this->beginContent('//layouts/guestmain');
+ ?>
 <div id="content">
 	<?php echo $content; ?>
 </div><!-- content -->
-<?php $this->endContent(); ?>
+<?php
+if (!Yii::app()->user->isGuest) {
+	$this->endContent();
+} else
+	$this->endContent();
+?>
