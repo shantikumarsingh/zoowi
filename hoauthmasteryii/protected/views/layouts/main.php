7f2901a55920c3e882739dfea736342978fba859
kumarsunnyil <kumar.sunnyil@gmail.com>
2015-03-14 01:09:07 +0530

    The Landing Page has been added
    The Landing Page has been added, with support for Twitter and Facebook

--- hoauthmasteryii/protected/views/layouts/main.php
+++ hoauthmasteryii/protected/views/layouts/main.php
@@ -12,99 +12,84 @@
 		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/custom.css">
 		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
 		<!--[if lt IE 8]>
-		<link rel="stylesheet" type="text/css" href="<?php //echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
 		<![endif]-->
 
-		<!--<link rel="stylesheet" type="text/css" href="<?php //echo Yii::app()->request->baseUrl; ?>/css/main.css">
-		<link rel="stylesheet" type="text/css" href="<?php //echo Yii::app()->request->baseUrl; ?>/css/form.css">-->
+		<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">-->
+		
 
 		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	</head>
 
-	<body>
-		<div class="wrapper jsc-sidebar-content jsc-sidebar-pulled">
-			<nav>
-				<a href="#" class="fa fa-bars link-menu jsc-sidebar-trigger"></a>
-			</nav>
-		
+<body>
 
-			<div class="container-fluid" id="page">
+<div class="container" id="page">
 
+	<div id="header">
+		<div id="logo"> <?php echo CHtml::encode(Yii::app()->name); ?></div>
+	</div><!-- header -->
 
-				<div id="header">
-					<div id="logo"> <?php echo CHtml::encode(Yii::app()->name); ?></div>
-				</div>
+	<div id="mainmenu">
+		<?php
+					$this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+			array('label'=>'Social Apps', 'url'=>array('/site/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Facebook', 'url'=>array('/fb/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Twitter', 'url'=>array('/twitter/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Instagram', 'url'=>array('/instagram/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Foursquare', 'url'=>array('/foursquare/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Tumblr', 'url'=>array('/tumblr/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Linkedin', 'url'=>array('/linkedin/index'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Profile', 'url'=>array('/user/profile'), 'visible'=>!Yii::app()->user->isGuest),
+			array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
 
-				<div id="mainmenu">
-					<?php
-						$this->widget('zii.widgets.CMenu',array(
-							'items'=>array(
-							array('label'=>'Social Apps', 'url'=>array('/site/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Facebook', 'url'=>array('/fb/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Twitter', 'url'=>array('/twitter/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Instagram', 'url'=>array('/instagram/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Foursquare', 'url'=>array('/foursquare/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Tumblr', 'url'=>array('/tumblr/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Linkedin', 'url'=>array('/linkedin/index'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Profile', 'url'=>array('/user/profile'), 'visible'=>!Yii::app()->user->isGuest),
-							array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-							),
-						)); 
-					?>
-				</div>
-
-				<?php if(isset($this->breadcrumbs)):?>
-					<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-						'links'=>$this->breadcrumbs,
-					)); ?>
-				<?php endif?>
-
-				<?php echo $content; ?>
 			
-				
+			//array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest),
+			//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+			//array('label'=>'Contact', 'url'=>array('/site/contact')),
+			//array('label'=>'Register', 'url'=>array('/user/registration'), 'visible'=>Yii::app()->user->isGuest),
+			// Using Submenu in yii 
+			/*
+			array('label'=>'SubMenu', 'url'=>array('/site/page', 'view'=>'about'), 
+				'items' =>array(
+					array('label'=>'Level 4 One', 'url'=>array('product/new')),
+                    array('label'=>'Level 4 Two', 'url'=>array('product/index')),
+				),
+				),
+				*/
+			),
+			)); 
+		?>
+	</div><!-- mainmenu -->
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
 
-			</div>
-		</div>
-		<nav class="sidebar jsc-sidebar" id="jsi-nav" data-sidebar-options="">
-			<ul class="sidebar-list">
-				<li><a href="./" class="current">SidebarJS</a></li>
-				<li><a href="http://github.com/makotot/sidebar/">View on Github</a></li>
-				<li><a href="http://github.com/makotot/sidebar/releases">Download</a></li>
-			</ul>
-		</nav>
-		<footer id="footer">
-			<nav class="navbar navbar-default navbar-fixed-bottom">
-				<div class="container-fluid">
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-							<li><a href="javascript:void(0);">Terms of Use</a></li>
-							<li><a href="javascript:void(0);">Privacy Policy</a></li>
-						</ul>
-						<p class="navbar-text pull-right">Copyright &copy; <?php echo date('Y'); ?> ZOOWIMAMA. All Rights Reserved.</p>
-					</div>
-				</div>
-			</nav>
-		</footer>
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		All Rights Reserved.<br/>
+		<?php echo Yii::powered(); ?>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+		<!--
+
+		Adding Javascript -- place them as required
+		-->
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.min.js"></script>
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/modernizr.js"></script>
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
-		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/sidebar.js"></script>
-		<script>
-			$('#jsi-nav').sidebar({
-				trigger: '.jsc-sidebar-trigger',
-				scrollbarDisplay: true,
-				pullCb: function () { console.log('pull'); },
-				pushCb: function () { console.log('push'); }
-			});
+		<!--<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/custom.js"></script>-->
 
-			$('#api-push').on('click', function (e) {
-				e.preventDefault();
-				$('#jsi-nav').data('sidebar').push();
-			});
-			$('#api-pull').on('click', function (e) {
-				e.preventDefault();
-				$('#jsi-nav').data('sidebar').pull();
-			});
-		</script>
-	</body>
+
+
+</body>
 </html>
--- hoauthmasteryii/protected/views/site/index.php
+++ hoauthmasteryii/protected/views/site/index.php
@@ -4,12 +4,318 @@
 $this->pageTitle=Yii::app()->name;
 $this->widget('ext.hoauth.widgets.HOAuth');
 
+function shuffle_assoc($list) { 
+	if (!is_array($list)) return ;
+	if(count($list)==0) return; 
+	
+	$keys = array_keys($list); 
+	shuffle($keys); 
+	$random = array(); 
+	foreach ($keys as $key) { 
+		$random[$key] = $list[$key]; 
+	}
+	return $random; 
+}
+
+	/**
+	 * 
+	 * converts timestamp to human readable format
+	 * @param String $dt
+	 */
+	function timestamp_to_relative_time( $dt )
+	{
+		$precision = 2;
+		$times=array(	365*24*60*60	=> "year",
+				30*24*60*60	=> "month",
+				7*24*60*60	=> "week",
+				24*60*60	=> "day",
+				60*60		=> "hour",
+				60		=> "minute",
+				1		=> "second");
+
+		$passed=time()-$dt;
+
+		if($passed<5)
+		{
+			$output='less than 5 seconds ago';
+		}
+		else
+		{
+			$output=array();
+			$exit=0;
+			foreach($times as $period=>$name)
+			{
+				if($exit>=$precision || ($exit>0 && $period<60)) 	break;
+				$result = floor($passed/$period);
+
+				if($result>0)
+				{
+					$output[]=$result.' '.$name.($result==1?'':'s');
+					$passed-=$result*$period;
+					$exit++;
+				}
+
+				else if($exit>0) $exit++;
+
+			}
+			$output=implode(' and ',$output).' ago';
+		}
+
+		return $output;
+	}
+	
+
+	
+	function customFBPosts($hybridauth){
+	
+		$fbAdapter = $hybridauth->authenticate( 'facebook');
+		$fbTimelineFeeds = $fbAdapter->api()->api('/me/posts');
+		
+		global $universalCount;
+		$currentFeed = array();
+	//	echo "<pre>" ;print_r($fbTimelineFeeds) ; exit;
+		foreach($fbTimelineFeeds['data']  as $feed){
+			$universalCount++;
+			if(isset($feed['message'])){
+					
+					$currentFeed[$universalCount] = array(
+						'provider'=>'facebook',
+						'message' =>array(
+							'id'=>	$feed['id'],
+							'messagetype' =>'message',
+							'message'=>	$feed['message'],
+							'actions'=>$feed['actions'],
+							'type'=>$feed['type'],
+							
+						),
+					);
+				if(isset($feed['comments']))
+					$currentFeed[$universalCount]['message']['comments'] = $feed['comments'];
+			}
+			if(isset($feed['story'])){
+				$currentFeed[$universalCount] = array(
+					'provider'=>'facebook',
+					'message' =>array(
+						'id'=>	$feed['id'],
+						'messagetype' =>'story',
+						'message'=>	$feed['story'],
+						'actions'=>$feed['actions'],
+						'type'=>$feed['type'],
+					),
+				);
+				if(isset($feed['picture']))
+					$currentFeed[$universalCount]['message']['picture'] = $feed['picture'];
+				if(isset($feed['comments']))
+					$currentFeed[$universalCount]['message']['comments'] = $feed['comments'];
+				if(isset($feed['name']))
+					$currentFeed[$universalCount]['message']['name'] = $feed['name'];
+			}	
+		}
+		$currentFeed = shuffle_assoc($currentFeed) ;	
+		return($currentFeed) ;	
+	}
+/**
+ * 
+ * Fetch the users timeline and construct the array
+ * @param unknown_type $hybridauth
+ */
+
+	function customTwitterPosts($hybridauth){
+	
+		global $universalCount;
+		$twitter= $hybridauth->authenticate( 'twitter');
+		/**
+		 * All these options work
+		 * $user_time_line = $twitter->api()->api('/statuses/home_timeline.json');
+		 * $user_time_line = $twitter->api()->api('/statuses/mentions_timeline.json');
+		 */
+		$user_time_line = $twitter->api()->api('/statuses/user_timeline.json');
+
+		
+
+		foreach ($user_time_line as $key=> $timeLine) {
+			$universalCount++; 
+			$currentFeed[$universalCount] = array(
+			'provider'=>'twitter',
+			'message' =>array(
+				'messagetype' =>'message',
+				'id' =>$timeLine->id_str ,
+				'message' =>$timeLine->text ,
+				'retweet'=>(isset($timeLine->retweeted_status))?$timeLine->retweeted_status->retweet_count:'' ,//$timeLine->retweet_count,
+				'name'=>$timeLine->user->name,
+				'favourite'=>$timeLine->favorite_count,
+				'createdTime'=>$timeLine->created_at,
+				'profileImageUrl'=>$timeLine->user->profile_image_url_https,
+				),
+			);
+			
+		}
+		$currentFeed = shuffle_assoc($currentFeed) ;
+		return $currentFeed;
+	}
+
+/**
+ * 
+ * Displays the post from Facebook page.
+ * @param $messagePost
+ */
+	function formatFbMessage($messagePost){
+	
+		$html = '
+			<div type="button" class="row" data-toggle="modal" data-target="#'.$messagePost['id'].'">
+			  '.$messagePost['message'].'
+			</div>
+				<div class="modal fade" id="'.$messagePost['id'].'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			  <div class="modal-dialog">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+			      </div>
+			      <div class="modal-body">
+			        '.$messagePost['message'];
+	
+			    if(isset($messagePost['comments'])){  
+				$html .= '<div class="row"> ';
+					foreach($messagePost['comments']['data'] as $comment) {
+						$html .= ' <div class="col-md-8"> ' . $comment['message'].'</div>' ;
+						$html .= '  <div class="col-md-8"> Created : ' . timestamp_to_relative_time(strtotime($comment['created_time'])).' </div>';
+						$html .= '  <div class="col-md-8"> Like : ' . $comment['like_count'].' </div>';
+					}
+				$html .= '</div> ';
+			    }
+			    if(isset($messagePost['messagetype']) && $messagePost['messagetype'] == 'story'){  
+					$html .= '<div class="row"> ';
+					$html .= '<div class="col-md-8"> <img style="width:32px;height=32px" src="'.$messagePost['picture'].'" /> </div>' ;
+					$html .= ' <div class="col-md-8"> ' . $messagePost['name'].'</div>' ;
+					if(isset($messagePost['comments'])) 		
+						foreach($messagePost['comments']['data'] as $comment) {
+						$html .= ' <div class="col-md-8"> ' . $comment['message'].'</div>' ;
+						$html .= '  <div class="col-md-8"> Like : ' . $comment['like_count'].' </div>';
+						$html .= '  <div class="col-md-8"> Created : ' . timestamp_to_relative_time(strtotime($comment['created_time'])).' </div>';
+						}
+					$html .= '</div> ';
+			    }
+				$html .=
+			        '
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-primary">Save changes</button>
+			      </div>
+			    </div>
+			  </div>
+			</div>';
+		return $html;
+	}
+
+/**
+ * Construct the HTML Data 
+ * @param unknown_type $fbPost
+ * @return $fbHtml
+ */
+	function htmlizeFbData($fbPost){
+		$fbHtml ='';
+		foreach ($fbPost as $post){
+			if($fbPost['message']['messagetype'] == 'message' || $fbPost['message']['messagetype'] == 'story'){
+				if(isset($post['message']))
+				$fbHtml .=	formatFbMessage($post);
+			}
+	
+		if($fbPost['message']['messagetype'] == 'story'){
+				if(isset($post['message']))
+					$fbHtml .=formatFbMessage($post);
+			}
+		}
+		return $fbHtml; 
+	}
+/**
+ * Constructs the HTML body and returns to the HTMLizer Function 
+ * 
+ */
+	function formatTwitterMessage($messagePost){
+		$html ='';
+		if(isset($messagePost['message'])) {
+			$html = '
+				<div type="button" class="row" data-toggle="modal" data-target="#'.$messagePost['id'].'">
+				  '.$messagePost['message'].'
+				</div>
+					<div class="modal fade" id="'.$messagePost['id'].'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				  <div class="modal-dialog">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+				      </div>
+				      <div class="modal-body">
+				        '.$messagePost['message'];
+					$html .= '<div class="row">';
+					$html .= '  <div class="col-md-8"> <img style="width:32px;heoght:32px" src="' . $messagePost['profileImageUrl'].'"> </div>';
+					$html .= '  <div class="col-md-8"> Tweet Count : ' . $messagePost['retweet'].' </div>';
+					$html .= '  <div class="col-md-8"> Favourite: ' . (isset($messagePost['favourite']))?$messagePost['favourite']:''.' </div>';
+					$html .= '  <div class="col-md-8"> ' . timestamp_to_relative_time(strtotime($messagePost['createdTime'])).' </div>';
+					$html .= '</div> ';
+					$html .=
+				      '</div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>';
+		}
+		return $html;
+	}	
+
+function htmlizeTwitterData($twitterPost){
+	$twitterHtml='';
+	foreach($twitterPost as $post){
+		$twitterHtml .= formatTwitterMessage($post);
+	}
+	
+	return $twitterHtml;
+}
+
 ?>
 <?php 
+	$config =  './protected/config/hoauth.php';
+	
+	require_once( "./protected/extensions/hoauth/hybridauth/Hybrid/Auth.php" );
+	//echo "<pre>" ;
+	$hybridauth = new Hybrid_Auth( $config );
+	$universalPosts = array();
+	$universalCount;
+	// inititalize the arrays 	
+	$fbPosts = array();
+	$twitterPosts = array();
+	
+	/*
+	 * Working Copy facebook 
+	*/
+	
+	// customize the facebook post to be displayed to the user.
+//	$fbPosts = customFBPosts($hybridauth);
+	// get the count of post available for display
+//	$universalCount = count($fbPosts);
 
+	$twitterPosts= customTwitterPosts($hybridauth);
+	$universalCount = count($twitterPosts);
+	
+	$finalResult = array_merge($fbPosts, $twitterPosts);
+	
+	$finalResult = shuffle_assoc($finalResult) ; 
+	//print_r($finalResult); exit;
+	foreach( $finalResult as $post){
+	
+		/**
+		 * formats the Post and displays as plain HTML
+		 */
+		if($post['provider'] == 'facebook'){
+			//htmlizeFbData($post);
+		}
+		if($post['provider'] == 'twitter'){
+			echo htmlizeTwitterData($post);
+		}
+	}
+	
 ?>
 
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Design the Page Here .</p>
